e76eaeabdb331c1ea6764a7be1b2d0d0d9f67c85
Davi açola <daviacola1@gmail.com>
2023-07-23 19:45:35 -0300

    appTrasportes

--- lib/mapa.dart
+++ lib/mapa.dart
@@ -1,4 +1,3 @@
-import 'package:find_transportes/widget.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -33,13 +32,100 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: 
-        GoogleMap(
+      body: Stack(
+        alignment: Alignment.topCenter,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(30),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    decoration: const InputDecoration(), // Substitua pela decoração desejada
+                  ),
+                ),
+                IconButton(
+                  icon: const Icon(
+                    Icons.menu,
+                    size: 30,
+                  ),
+                  onPressed: () {},
+                )
+              ],
+            ),
+          ),
+          // ignore: avoid_unnecessary_containers
+          Container( child:
+            GoogleMap(
           onMapCreated: _onMapCreated,
          initialCameraPosition: CameraPosition(
            target: LatLng(lat, long),
            zoom: 11.0
          ),
-        ));
+        )
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Color.fromARGB(141, 255, 255, 255),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(0),
+                  topRight: Radius.circular(0),
+                ),
+              ),
+              height: 80,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Column(
+                    children: [
+                      IconButton(
+                        icon: const Icon(
+                          Icons.location_on,
+                          color: Colors.blue, // Substitua pela cor desejada
+                          size: 30,
+                        ),
+                        onPressed: () => {},
+                      ),
+                     const  Text(
+                        "Navegar",
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.blue, // Substitua pela cor desejada
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      IconButton(
+                        icon: const Icon(
+                          Icons.near_me,
+                          color: Colors.grey,
+                          size: 30,
+                        ),
+                        onPressed: () => {},
+                      ),
+                      const Text(
+                        "Viajar",
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
